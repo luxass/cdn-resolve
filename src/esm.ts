9a58d80df14aa506e2307294974b3928b8a8cc49
Lucas Nørgård <lucasnrgaard@gmail.com>
2023-03-28 15:34:37 +0200

    added more esm options

--- src/esm.ts
+++ src/esm.ts
@@ -1,3 +1,6 @@
+/**
+ * https://esm.sh/#docs
+ */
 export interface ESMOptions {
   pin?: string;
   esbuild?: {
@@ -5,6 +8,34 @@ export interface ESMOptions {
     keepNames?: boolean;
     ignoreAnnotations?: boolean;
   };
+  deps?: string[];
+  /**
+   * A list of dependencies to externalize
+   * if true, all dependencies will be externalized
+   */
+  external?: string[] | boolean;
+  /**
+   * A list of modules to alias to other modules
+   */
+  alias?: Record<string, string>;
+  /**
+   * To enable tree shaking, you need to specify the list of exports to keep
+   */
+  treeShake?: string[];
+  /**
+   * Bundle
+   */
+  bundle?: boolean;
+
+  /**
+   * Development Mode
+   */
+  dev?: boolean;
+
+  /**
+   * Worker Mode
+   */
+  worker?: boolean;
 }
 
 export async function resolveESM(module: string, options?: ESMOptions) {
