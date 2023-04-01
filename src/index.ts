0edb627cfaa951d040e51a05eedb5201c091b5fa
Lucas Nørgård <lucasnrgaard@gmail.com>
2023-04-01 08:46:45 +0200

    added resolveCDN function

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,35 @@
-export {
-  type SkypackOptions,
+import { type ESMOptions, resolveESM, resolveESMTypes } from "./esm";
+import { resolveJSDelivr } from "./jsdelivr";
+import {
   type SkypackHeaders,
+  type SkypackOptions,
   resolveSkypack,
   resolveSkypackHeaders
 } from "./skypack";
-export { type ESMOptions, resolveESM } from "./esm";
-export { type UnpkgOptions, resolveUnpkg } from "./unpkg";
-export { resolveJSDelivr } from "./jsdelivr";
+import { resolveUnpkg } from "./unpkg";
 
 export { type ParsedPackage, parsePackage } from "./utils";
+export {
+  resolveESM,
+  resolveESMTypes,
+  resolveJSDelivr,
+  resolveSkypack,
+  resolveSkypackHeaders,
+  resolveUnpkg
+};
+export type { ESMOptions, SkypackHeaders, SkypackOptions };
+
+export function resolveCDN(cdn: "skypack" | "esm" | "unpkg" | "jsdelivr") {
+  switch (cdn) {
+    case "skypack":
+      return resolveSkypack;
+    case "esm":
+      return resolveESM;
+    case "unpkg":
+      return resolveUnpkg;
+    case "jsdelivr":
+      return resolveJSDelivr;
+    default:
+      throw new Error(`Unknown CDN: ${cdn}`);
+  }
+}
