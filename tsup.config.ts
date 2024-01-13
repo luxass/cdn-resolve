15047e5d2f29998601bf7725da670171f55dd8cb
Lucas Nørgård <lucasnrgaard@gmail.com>
2024-01-13 15:55:00 +0100

    fix: use correct exports

--- tsup.config.ts
+++ tsup.config.ts
@@ -1,9 +1,15 @@
 import { defineConfig } from "tsup";
 
 export default defineConfig({
-  entry: ["src/*.ts"],
+  entry: ["./src/*.ts"],
   format: ["cjs", "esm"],
   clean: true,
-  treeshake: true,
   dts: true,
+  treeshake: true,
+  bundle: true,
+  outExtension(ctx) {
+    return {
+      js: ctx.format === "cjs" ? ".cjs" : ".mjs",
+    };
+  },
 });
